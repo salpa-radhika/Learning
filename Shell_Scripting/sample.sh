b415d8d8380930421978f94ddb3c72b2d5752815
Salpa <salparadhika1997@gmail.com>
2023-05-30 14:02:00 -0500

    Sample sh file

---
+++ Shell_Scripting/sample.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#comments
+
+echo "this is my first sample sh file"
+
+#creating directory
+
+mkdir Sample
+
+#change directory
+
+cd Sample
+
+#create two txt files
+
+touch file1.txt file2.txt                            
