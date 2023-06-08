3c9ff2c5aaf45db766487e91a5e200f2586d1309
Salpa <salparadhika1997@gmail.com>
2023-06-08 17:18:09 -0500

    files added

--- Terraform/AWS-EC2/resource.tf
+++ Terraform/AWS-EC2/resource.tf
@@ -4,8 +4,13 @@ resource "aws_instance" "terraform_instance" {
   ami           = "ami-092b51d9008adea15"
   instance_type = "t2.micro"
   
-  key_name = "client_server"
+  key_name = var.ami
   tags = {
   name = "terraform-instance"
   }
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "${var.key_name}"
+  public_key = "${var.public_key}"
 }
